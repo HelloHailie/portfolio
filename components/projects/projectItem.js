347287bee115da2b21e430a80992d3e6d8c2d34f
HelloHailie <alsltjsals@gmail.com>
2022-11-02 14:16:41 +0900

    feat:프로젝트 아이템 정리

---
+++ components/projects/projectItem.js
@@ -0,0 +1,73 @@
+import Image from "next/image";
+
+export default function ProjectItem({ data }) {
+  const title = data.properties.Name.title[0].plain_text;
+  const description = data.properties.Description.rich_text[0].text.content;
+  const github = data.properties.Github.url;
+  const utube = data.properties.Utube.url;
+  const imgSrc = data.cover.file?.url || data.cover.external.url;
+  const tags = data.properties.Tags.multi_select;
+  const startDate = data.properties.WorkPeriod.date.start;
+  const endDate = data.properties.WorkPeriod.date.end;
+
+  const calculatedPeriod = (start, end) => {
+    const startDateStringArray = start.split("-");
+    const endDateStringArray = end.split("-");
+
+    const startDate = new Date(
+      Number(startDateStringArray[0]),
+      Number(startDateStringArray[1]),
+      Number(startDateStringArray[2])
+    ).valueOf();
+    const endDate = new Date(
+      Number(endDateStringArray[0]),
+      Number(endDateStringArray[1]),
+      Number(endDateStringArray[2])
+    ).valueOf();
+
+    console.log(`${startDate}`);
+    console.log(`${endDate}`);
+
+    const diffInMs = Math.abs(endDate - startDate);
+    const result = diffInMs / (1000 * 60 * 60 * 24);
+
+    return result + 1;
+  };
+
+  return (
+    <div className='project-card'>
+      <Image
+        className='rounded-t-xl'
+        src={imgSrc}
+        width='100'
+        height='100'
+        alt='project cover image'
+        layout='responsive'
+        objectFit='none'
+        quality={100}
+      />
+      <div className='p-4 flex flex-col'>
+        <h1 className='text-2xl font-bold'>{title}</h1>
+        <h3 className='mt-4'>{description}</h3>
+
+        <a href={github}>깃허브 링크 바로가기</a>
+        <a href={utube}>유튜브 시연영상 바로가기</a>
+        <p className='my-2'>
+          작업 기간 : {startDate} ~ {endDate} (
+          {calculatedPeriod(startDate, endDate)}
+          일)
+        </p>
+        <div className='flex items-start mt-2 md:overflow-auto sm:overflow-auto'>
+          {tags.map((aTag) => (
+            <h1
+              className='px-1 py-1 mr-2 rounded-md bg-sky-200 dark:bg-sky-700 w-30 '
+              key={aTag.id}
+            >
+              {aTag.name}
+            </h1>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/projects.js
+++ pages/projects.js
@@ -2,65 +2,98 @@ import Layout from "../components/layout";
 import Head from "next/head";
 import { TOKEN, DATABASE_ID } from "../config";
 import axios from "axios";
+import ProjectItem from "../components/projects/projectItem";
 
-export default function Projects() {
+export default function Projects({ projects }) {
+  // console.log(projects);
   return (
     <Layout>
-      <Head>
-        <title>이선민 포트폴리오</title>
-        <meta name='description' content='이선민 포트폴리오' />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-      <h1>프로젝트</h1>
+      <div className='flex flex-col items-center justify-center min-h-screen mb-10 px-10'>
+        <Head>
+          <title>이선민 포트폴리오</title>
+          <meta name='description' content='이선민 포트폴리오' />
+          <link rel='icon' href='/favicon.ico' />
+        </Head>
+        <h1 className='text-4xl sm:text-6xl py-5 flex justify-center '>
+          <span className='text-blue-700 font-bold'>
+            {projects.results.length}
+          </span>
+          개의 프로젝트가 있습니다.
+        </h1>
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-8 m-6 py-10 w-full'>
+          {projects.results.map((aProject) => (
+            <ProjectItem data={aProject} key={aProject.id} />
+          ))}
+        </div>
+      </div>
     </Layout>
   );
 }
 
 export async function getStaticProps() {
   const options = {
-    method: "GET",
+    method: "POST",
     headers: {
       accept: "application/json",
       "Notion-Version": "2022-02-22",
+      "content-type": "application/json",
       authorization: `Bearer ${TOKEN}`,
     },
+    body: JSON.stringify({
+      sorts: [
+        {
+          property: "Name",
+          direction: "ascending",
+        },
+      ],
+      page_size: 100,
+    }),
   };
 
   const res = await fetch(
-    `https://api.notion.com/v1/databases/${DATABASE_ID}`,
+    `https://api.notion.com/v1/databases/${DATABASE_ID}/query`,
     options
   );
 
   const projects = await res.json();
 
-  // const projectIds = projects.results.map((aProject) => aProject.id);
-  // console.log(projectIds);
+  const projectNames = projects.results.map((aProject) =>
+    aProject.properties.Name.title.map((e) => e.plain_text)
+  );
+
+  // console.log(`projectNames : ${projects}`)
+  console.log(projects.length);
 
   // const options = {
-  //   method: "GET",
-  //   url: `https://api.notion.com/v1/databases/${DATABASE_ID}`,
+  //   method: "POST",
+  //   url: `https://api.notion.com/v1/databases/${DATABASE_ID}/query`,
   //   headers: {
   //     accept: "application/json",
   //     "Notion-Version": "2022-02-22",
+  //     "content-type": "application/json",
   //     authorization: `Bearer ${TOKEN}`,
   //   },
+  //   data: { page_size: 100 },
   // };
 
   // await axios
   //   .request(options)
   //   .then(function (response) {
   //     const projects = response.data;
+
   //     console.log(projects);
 
-  //     const projectIds = projects.map((aProject) => aProject.id);
+  //     const projectIds = projects.results.map(
+  //       (aProject) => aProject.properties.Name.title
+  //     );
 
-  //     console.log(`projectIds: ${projectIds}`);
+  //     console.log(`projectIds : ${projectIds}`);
   //   })
   //   .catch(function (error) {
   //     console.error(error);
   //   });
 
   return {
-    props: { mods: projects }, // will be passed to the page component as props
+    props: { projects }, // will be passed to the page component as props
   };
 }
