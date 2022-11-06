2ccbcd54d1226f00406cb2cbe475b883cb5a53dd
HelloHailie <alsltjsals@gmail.com>
2022-11-06 16:52:30 +0900

    fix:key 속성 추가

--- components/skills-item.js
+++ components/skills-item.js
@@ -14,32 +14,38 @@ import Vercel from "../public/svg/vercel.svg";
 
 const frontSkillsItem = [
   [
-    <React />,
+    <React key={React} />,
     "React",
     "다양한 라이브러리를 사용할 수 있는 React를 주로 사용합니다.",
   ],
   [
-    <Javascript />,
+    <Javascript key={Javascript} />,
     "Javascript",
     "ECMAScript의 변화를 꾸준히 살피고 학습합니다.",
   ],
   [
-    <Nextjs />,
+    <Nextjs key={Nextjs} />,
     "Nextjs",
     "초기 로딩 속도가 빠르고 검색 엔진 최적화(SEO)에 유리한 SSR에 관심이 생겨 공부하고 있습니다.",
   ],
   [
-    <Image src={zustand} alt='zustand image' width={50} height={50} />,
+    <Image
+      src={zustand}
+      alt='zustand image'
+      width={50}
+      height={50}
+      key={Zustand}
+    />,
     "Zustand",
     "사용하기 쉽고, 불필요한 렌더링을 줄일 수 있는 Zustand를 사용하여 상태관리를 한 경험이 있습니다.",
   ],
   [
-    <StyledComponet />,
+    <StyledComponet key={StyledComponet} />,
     "Styled-component",
     "컴포넌트 별로 바로 CSS를 작성하여 직관적으로 확인할 수 있어서 React를 사용한 프로젝트에서 사용한 경험이 있습니다.",
   ],
   [
-    <TailwindCSS />,
+    <TailwindCSS key={TailwindCSS} />,
     "TailwindCSS",
     " html에서 클래스명 하나로 style이 지정 가능하여 Nest.js를 사용한 프로젝트에서 tailwind CSS를 사용한 경험이 있습니다.",
   ],
@@ -47,23 +53,31 @@ const frontSkillsItem = [
 
 const etcSkillsItem = [
   [
-    <Github />,
+    <Github key={Github} />,
     "Github",
     " Git Commit convention을 통한 협업 경험이 있고, 칸반 보드를 활용하여 업무 시각화 경험이 있습니다.",
   ],
   [
-    <Figma />,
+    <Figma key={Figma} />,
     "Figma",
     "Figma를 이용한 프로젝트 와이어 프레임, 유저 플로우 제작 경험이 있습니다.",
   ],
-  [<Discord />, "Discord", "디스코드를 이용한 소통, 협업 경험이 있습니다."],
   [
-    <Notion />,
+    <Discord key={Discord} />,
+    "Discord",
+    "디스코드를 이용한 소통, 협업 경험이 있습니다.",
+  ],
+  [
+    <Notion key={Notion} />,
     "Notion",
     "노션을 통한 프로젝트 일정 관리와 회의록 작성을 하여 팀 문서를 정리한 경험이 있습니다.",
   ],
-  [<Aws />, "AWS", "AWS S3을 통한 프로젝트 배포 경험이 있습니다."],
-  [<Vercel />, "Vercel", "Vercel을 통한 프로젝트 배포 경험이 있습니다."],
+  [<Aws key={Aws} />, "AWS", "AWS S3을 통한 프로젝트 배포 경험이 있습니다."],
+  [
+    <Vercel key={Vercel} />,
+    "Vercel",
+    "Vercel을 통한 프로젝트 배포 경험이 있습니다.",
+  ],
 ];
 
 export { frontSkillsItem, etcSkillsItem };
